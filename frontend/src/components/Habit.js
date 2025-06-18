5ece599aaccbae00e72a5779bcb4c317e6eec9c1
Nico <rossi2nick@gmail.com>
2025-06-17 23:42:42 -0400

    Modified Habit component and Habit page.

--- frontend/src/components/Habit.js
+++ frontend/src/components/Habit.js
@@ -7,12 +7,41 @@ export const Habit = ({ habit }) => {
  
   return (
     <div className = "habit">
-      <p>{habit.name}</p>
-      {/* <p>frequency: {habit.frequency}</p>
-      <p>streak: {habit.streak} </p> */}
-      <p>Synced with users: {syncedUsers.join(", ")}</p>
+      <div className = "left">
+        <h3>{habit.name}</h3>
+        {/* <p>frequency: {habit.frequency}</p>
+        <p>streak: {habit.streak} </p> */}
+        
+        <p className = "habit-id"> ID: {habit._id} </p>
 
-              
+        {habit.privacy === 2 ? (
+          <p className = "privacy"> Public </p>
+        ) : habit.privacy === 1 ? (
+          <p className = "privacy"> Friends </p>
+        ) : (
+          <p className = "privacy"> Private </p>
+        )}
+
+        {habit.frequency === 7 ? (
+          <p> Daily </p>
+        ) : habit.frequency === 1 ? (
+          <p> Weekly </p>
+        ) : (
+          <p> Custom frequency </p>
+        )}
+
+           
+      </div>
+      <div className = "right">
+        {syncedUsers.length > 0 ? (
+          <>
+            <p style = {{ marginBottom: "-10px" }}> Synced Users: </p>
+            <p style = {{ fontSize: "12px" }}> {syncedUsers.join(", ")} </p>
+          </>
+        ) : ( 
+          <p> No synced users </p>
+        )}  
+      </div>
     </div>
   )
 }

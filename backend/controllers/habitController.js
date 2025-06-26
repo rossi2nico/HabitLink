a11de1598576a3cf9b9fdfa54ba3d14b867c6445
Nico <rossi2nick@gmail.com>
2025-06-26 12:57:31 -0400

    Modified API endpoint for deleteHabit to remove deleted habit from other synced habits

--- backend/controllers/habitController.js
+++ backend/controllers/habitController.js
@@ -342,7 +342,6 @@ const getPublicHabits = async (req, res) => {
     res.status(200).json(habits)
 }
 
-// This needs to be modified to handle synced habits
 const deleteHabit = async (req, res) => {
     const { habitId } = req.params
 
@@ -350,13 +349,34 @@ const deleteHabit = async (req, res) => {
         return res.status(404).json({error: "Habit not found"})
     }
 
-    const habit = await Habit.findOneAndDelete({_id: habitId});
+    try {
+        const habit = await Habit.findById(habitId)
+        if (!habit) {
+            return res.status(404).json({error: "Habit not found"})
+        }
+        // iterate through synced arrays, find the habits and remove our habit from their synced list
+        const modifiedHabits = [];
 
-    if (!habit) {
-        return res.status(404).json({error: "Habit not found"})
+        for (const syncedHabit of habit.syncedHabits) {
+            const otherHabit = await Habit.findById(syncedHabit.habitId)
+            if (!otherHabit) continue;
+            
+            otherHabit.syncedHabits = otherHabit.syncedHabits.filter(
+                otherSyncedHabits => otherSyncedHabits.habitId.toString() !== habitId.toString()
+            )
+            await otherHabit.save();
+            modifiedHabits.push(otherHabit);
+        }
+
+        await habit.remove();
+        return res.status(200).json({
+            deletedHabit: habit,
+            updatedSyncedHabits: modifiedHabits
+        })
     }
-
-    res.status(200).json(habit)
+    catch (error) {
+        return res.status(400).json({ error: error.message })
+    }
 }
 
 /*

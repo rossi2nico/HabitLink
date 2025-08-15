51a6359a68b4f4ff8980b0e579b2cb658064e016
Nico <rossi2nick@gmail.com>
2025-08-15 15:35:32 -0400

    prev and next buttons for calendar

--- frontend/src/components/Calendar.js
+++ frontend/src/components/Calendar.js
@@ -1,21 +1,36 @@
 import { useState } from 'react';
-import { eachDayOfInterval, endOfMonth, format, startOfMonth, startOfToday, startOfWeek, endOfWeek, isSameMonth } from 'date-fns'
+import { eachDayOfInterval, endOfMonth, format, startOfMonth, startOfToday, startOfWeek, endOfWeek, isSameMonth, isSameDay, isAfter, parse, add } from 'date-fns'
 
 export const Calendar = ({ habit }) => {
-
 
   let today = startOfToday()
   let newDays = eachDayOfInterval({ 
     start: startOfWeek(startOfMonth(today)), 
     end: endOfWeek(endOfMonth(today)) })
 
+  const [currentMonth, setCurrentMonth] = useState(format(today, 'MMMM-yyyy'))
+
+  let firstDayCurrentMonth = parse(currentMonth, 'MMMM-yyyy', new Date())
+
+  const nextMonth = () => {
+    let firstDayNextMonth = add(firstDayCurrentMonth, { months: 1 })
+    if (!isAfter(firstDayNextMonth, today)) {
+      setCurrentMonth(format(firstDayNextMonth, 'MMMM-yyyy'))
+    }
+  }
+
+  const prevMonth = () => {
+    let firstDayPrevMonth = add(firstDayCurrentMonth, { months: -1 })
+    setCurrentMonth(format(firstDayPrevMonth, 'MMMM-yyyy'))
+  }
+
   return (
     <div className = "calendar">  
       <header>
-        <button></button>
-        <p> { format(today, 'MMMM yyyy') } </p>
-        <button>
-          
+        <button onClick = {() => prevMonth()}>prev</button>
+        <p> { format(firstDayCurrentMonth, 'MMMM yyyy') } </p>
+        <button onClick = {() => nextMonth()}>
+          next
         </button>
       </header>
       <div className = "days">
@@ -26,7 +41,13 @@ export const Calendar = ({ habit }) => {
           
           <div 
             key={day.toString()} 
-            style={{ border: '1px solid gray' }} 
+            style={{
+              color: isSameDay(today, day) 
+                ? '#53e7b6fb' 
+                : isAfter(day, today) 
+                  ? '#666' 
+                  : 'white'
+            }}
             className='calendar-day'
           >
             <time

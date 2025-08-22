920fe73f2884486f6ef25da2a2cf0977be406db8
Nico <rossi2nick@gmail.com>
2025-08-22 19:07:14 -0400

    fixed navigation alignments

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -8,29 +8,26 @@ export const Navigation = () => {
 
   return (
     <div className="navigation">
-  <div className="nav-left">
-    <h3 className = "habitlink"> HabitLink </h3>
-  </div>
-  <div className = "nav-mid">
-    <h3><Link to="/habits">Habits</Link></h3>
-    <h3><Link to="/friends">Friends</Link></h3>
-  </div>
-
-  <div className="nav-right">
-    {!user ? (
-      <>
-        <h3><Link to="/login">Login</Link></h3>
-        <h3><Link to ="/signup">Register</Link></h3>
-      </>
-    ) : (
-      <>
-        <h3 className = "nav-user">{user.username}</h3>
-        <button className = "logout" onClick = { logout }>logout</button>
-      </>
-    )}
-  </div>
-</div>
-
-
+      <div className="nav-left">
+        <h3 className="habitlink"> HabitLink </h3>
+      </div>
+      <div className="nav-mid">
+        <h3><Link to="/habits">Habits</Link></h3>
+        <h3><Link to="/friends">Friends</Link></h3>
+      </div>
+      <div className="nav-right" style={{marginLeft: 'auto', marginRight: '-25px', display: 'flex', alignItems: 'center', gap: '1rem', justifyContent: 'flex-end', minWidth: '160px' }}>
+        {!user ? (
+          <>
+            <h3 className="login-nav" style={{ marginLeft: 'auto' }}><Link to="/login">Login</Link></h3>
+            <h3 className="register-nav"><Link to="/signup">Register</Link></h3>
+          </>
+        ) : (
+          <>
+            <h3 className="nav-user">{user.username}</h3>
+            <button className="logout" onClick={logout}>logout</button>
+          </>
+        )}
+      </div>
+    </div>
   );
 }
